4638b8c5d5b12d81c70f52332405af738a9210f5
Guthey <aguthey@ebay.com>
2017-03-23 15:29:18 -0400

    this is first commit

--- app/app.js
+++ app/app.js
@@ -1,14 +1,71 @@
 'use strict';
-
+ 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
+  ,'elasticui'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
   $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+}]).controller('MyController', function ($scope, userRepoService){
+     
+     var onFetchError = function(message){
+       $scope.error = "Error Fetching Users. Message:" +message;
+     };
+     
+     var onFetchCompleted = function(data){
+        $scope.Sellers =data;
+		$scope.Sources = [{
+                Id: 1,
+                Name: 'Inventory Received'
+            }, {
+                Id: 2,
+                Name: 'Inventory In Tool'
+            }, {
+                Id: 3,
+                Name: 'Inventory Sent'
+            }];
+     };
+     
+     var getUsers = function(){
+       userRepoService.get().then(onFetchCompleted,onFetchError);
+     };
+     
+     getUsers();     
+     
+   })
+            .constant('euiHost', 'http://localhost:9200'); // ACTION: change to cluster address
+			
+				(function(){
+    
+    var userRepoService = function($http){
+      
+      var getUsers = function(username){
+			return $http.get("http://localhost:8080/GetSellers/crunchify/getSellers")
+						   .then(function(response){
+                           return response.data; 
+                        });
+      };
+  
+      return {
+          get: getUsers
+      };
+        
+    };
+    
+    var module = angular.module("myApp");
+    module.factory("userRepoService", userRepoService);
+    
+}()) ;
+
+
+
+
+
+
+
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,14 +1,61 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.view1', ['ngRoute', 'elasticui'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
+    templateUrl: 'view1/demo.html',
+    controller: 'MyController1'
   });
 }])
 
-.controller('View1Ctrl', [function() {
-
-}]);
+.controller('MyController1', function ($scope, userRepoService){
+     
+     var onFetchError = function(message){
+       $scope.error = "Error Fetching Users. Message:" +message;
+     };
+      $scope.message="tst me";
+     var onFetchCompleted = function(data){
+        $scope.Sellers =data;
+		$scope.Sources = [{
+                Id: 1,
+                Name: 'Inventory Received'
+            }, {
+                Id: 2,
+                Name: 'Inventory In Tool'
+            }, {
+                Id: 3,
+                Name: 'Inventory Sent'
+            }];
+     };
+     
+     var getUsers = function(){
+       userRepoService.get().then(onFetchCompleted,onFetchError);
+     };
+     
+     getUsers();     
+     
+   })
+            .constant('euiHost', 'http://localhost:9200'); // ACTION: change to cluster address
+			
+				(function(){
+    
+    var userRepoService = function($http){
+      
+      var getUsers = function(username){
+			return $http.get("http://localhost:8080/GetSellers/crunchify/getSellers")
+						   .then(function(response){
+                           return response.data; 
+                        });
+      };
+  
+      return {
+          get: getUsers
+      };
+        
+    };
+    
+    var module = angular.module("myApp");
+    module.factory("userRepoService", userRepoService);
+    
+}());
